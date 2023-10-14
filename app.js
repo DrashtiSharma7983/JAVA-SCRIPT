2aec3ea35ed749493d6091cbc36ec6bac8e66505
Drashti Sharma <drashtisharma7983@gmail.com>
2023-10-14 07:33:36 +0530

    modified

--- app.js
+++ app.js
@@ -1,37 +1,8 @@
-// JS(Part3)PracticeQuestionsQs1.WriteaJavaScriptprogramtogetthefirstnelementsofanarray.
-// [ncanbeanypositivenumber].
-// Forexample:forarray[7,9,0,-2]and n=3Print,[7,9,0]
+let arr = [["Ram","Shyam","Geeta"],["Gauri","Kmkum","Jhanvi"]];
 
-// let arr = [7,9,0,-2];
-// let n = 2;
-// ans = arr.slice(0,n);
-// console.log(ans);
-
-// Qs2.WriteaJavaScriptprogramtogetthelastnelementsofanarray.[ncanbeanypositivenumber].
-// Forexample:forarray[7,9,0,-2]andn=3Print,[9,0,-2]
-// let arr = [7,9,0,-2];
-// let n= 3;
-// ans = arr.slice(arr.length-n);
-// console.log(ans);
-
-
-//Qs3.WriteaJavaScriptprogramtocheckwhetherastringisblankornot
-
-// let str = "Drashti";
-// if(str.length === 0){
-//     console.log("String is empty");
-// }
-// else{
-//     console.log("String is not empty");
-// }
-
-//Qs4.WriteaJavaScriptprogramtotestwhetherthecharacteratthegiven(character)indexislowercase.
-
-let str = "DrAsHtiShArma";
-let indx = 5;
-if(str[indx]==str[indx].toLowerCase()){
-    console.log("Character is in lower case");
-}
-else{
-    console.log("Character is not in lower case");
+for(let i=0;i<arr.length;i++){
+    console.log(i,arr[i]);
+    for(let j=0;j<arr[i].length;j++){
+        console.log(`j=${j}, ${arr[i][j]}`);
+    }
 }
--- pract.js
+++ pract.js
@@ -86,3 +86,40 @@ console.log("Welcome" , firstname ," " ,lastname, "!");
 //     console.log("It is not a good string");
 // }
 
+// JS(Part3)PracticeQuestionsQs1.WriteaJavaScriptprogramtogetthefirstnelementsofanarray.
+// [ncanbeanypositivenumber].
+// Forexample:forarray[7,9,0,-2]and n=3Print,[7,9,0]
+
+// let arr = [7,9,0,-2];
+// let n = 2;
+// ans = arr.slice(0,n);
+// console.log(ans);
+
+// Qs2.WriteaJavaScriptprogramtogetthelastnelementsofanarray.[ncanbeanypositivenumber].
+// Forexample:forarray[7,9,0,-2]andn=3Print,[9,0,-2]
+// let arr = [7,9,0,-2];
+// let n= 3;
+// ans = arr.slice(arr.length-n);
+// console.log(ans);
+
+
+//Qs3.WriteaJavaScriptprogramtocheckwhetherastringisblankornot
+
+// let str = "Drashti";
+// if(str.length === 0){
+//     console.log("String is empty");
+// }
+// else{
+//     console.log("String is not empty");
+// }
+
+//Qs4.WriteaJavaScriptprogramtotestwhetherthecharacteratthegiven(character)indexislowercase.
+
+// let str = "DrAsHtiShArma";
+// let indx = 5;
+// if(str[indx]==str[indx].toLowerCase()){
+//     console.log("Character is in lower case");
+// }
+// else{
+//     console.log("Character is not in lower case");
+// }
