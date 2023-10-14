8170fb01e0ec25d55e65a27465a3c48fe48c1be3
Drashti Sharma <drashtisharma7983@gmail.com>
2023-10-14 19:47:34 +0530

    modified

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
-let arr = [["Ram","Shyam","Geeta"],["Gauri","Kmkum","Jhanvi"]];
 
-for(let i=0;i<arr.length;i++){
-    console.log(i,arr[i]);
-    for(let j=0;j<arr[i].length;j++){
-        console.log(`j=${j}, ${arr[i][j]}`);
-    }
-}
+
--- pract.js
+++ pract.js
@@ -123,3 +123,14 @@ console.log("Welcome" , firstname ," " ,lastname, "!");
 // else{
 //     console.log("Character is not in lower case");
 // }
+
+
+// Qs1.WriteaJSprogramtodeletealloccurrencesofelement‘num’inagivenarray.Example:ifarr=[1,2,3,4,5,6,2,3]&num=2Resultshouldbearr=[1,3,4,5,6,3]
+let arr=[1,2,3,4,5,6,2,3];
+let num=2;
+ for (let i=0; i<arr.length; i++){
+    if(arr[i]==num){
+        arr.splice(i,1);
+    }
+ }
+ console.log("result is : ",arr);
