bc024e28acef3c0924dbef010fa13b3128626d6e
Drashti Sharma <drashtisharma7983@gmail.com>
2023-10-15 09:18:45 +0530

    modified

---
+++ app.js
@@ -0,0 +1,7 @@
+//generate a random number btw 1 and 100
+let num = Math.floor(Math.random()*100)+1;
+console.log(num);
+
+//generate a random number btw 1 and 5
+let num = Math.floor(Math.random()*5)+1;
+console.log(num);
--- pract.js
+++ pract.js
@@ -148,3 +148,12 @@ while(copy>0){
     copy = Math.floor(copy/10);
 }
 console.log(count);
+
+
+//generate a random number btw 1 and 100
+let num = Math.floor(Math.random()*100)+1;
+console.log(num);
+
+//generate a random number btw 1 and 5
+let num = Math.floor(Math.random()*5)+1;
+console.log(num);
