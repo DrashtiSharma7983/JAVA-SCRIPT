d7069d5cafa7de38cec5ee89a977e6064adb9d44
Drashti Sharma <drashtisharma7983@gmail.com>
2023-10-16 21:49:26 +0530

    modified

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-//Print the table using function
-function printTable(num){
-    for(let i=num;i<=num*10;i=i+num){
-        console.log(i);
-    }
+
+function diceRoll(){
+    let rslt = Math.floor(Math.random()*6)+1;
+    console.log(rslt);
 }
-printTable(7);
+diceRoll();
--- function.js
+++ function.js
@@ -1,3 +1,9 @@
+function diceRoll(){
+    let rslt = Math.floor(Math.random()*6)+1;
+    console.log(rslt);
+}
+diceRoll();
+
 //Print the table using function
 function printTable(num){
     for(let i=num;i<=num*10;i=i+num){
