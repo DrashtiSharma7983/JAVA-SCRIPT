0bc6534c59f678edc0b04094dfff0630835c70c2
Drashti Sharma <drashtisharma7983@gmail.com>
2023-10-18 23:00:16 +0530

    modified

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ function printInfo(name,age){
     console.log(`My name is ${name} and i am ${age} years old`);
 }
 printInfo("Drashti",24);
+
+function diceRoll(){
+    let rslt = Math.floor(Math.random()*6)+1;
+    console.log(rslt);
+}
+diceRoll();
