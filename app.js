bb96995bdbeb25f4d9bbcdfd73c2216b1507903e
Drashti Sharma <drashtisharma7983@gmail.com>
2023-10-19 13:31:47 +0530

    modified

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-//Qs4.Write a JavaScript function to count the number of vowels in a String argument.
-let str = "ilovemyplace";
+const student  = {
+    name:"drashti",
+    age:24,
+    prop:this,
+    getName: function(){
+        return this.name;
+    },
+};
 
-function countVowel(str){
-    let count = 0;
-    for(let i=0;i<str.length;i++){
-        if(
-            str.charAt(i) == 'a'||
-            str.charAt(i) == 'e'||
-            str.charAt(i) == 'i'||
-            str.charAt(i) == 'o'||
-            str.charAt(i) == 'u'
-        ){
-            count ++;
-        }
-    }
-    return count;
-}
