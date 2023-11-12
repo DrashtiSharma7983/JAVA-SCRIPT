05732855b58d545c102288c3dadb37c4a2195a95
Drashti Sharma <drashtisharma7983@gmail.com>
2023-11-12 08:43:30 +0530

    modified

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let form = document.querySelector('form');
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log("form submitted");
-    alert("form submitted");
+    let inp = document.querySelector('input');
+    console.dir(inp);
+    console.log(inp.value);
 })
