9feca392f65a2db6cfa2a6000e9baf873e81d162
Drashti Sharma <drashtisharma7983@gmail.com>
2023-12-25 18:16:35 +0530

    modified

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-async function greet(){
-    //throw "page not found";
-    return "hello!";
-}
+let url = "https://catfact.ninja/fact";
 
-greet()
-.then((result)=>{
-    console.log("promise was resolved");
-    console.log("result is:", result);
-})
-
-.catch((err)=>{
-    console.log("promise was rejected with error:", err);
-});
+fetch(url);
